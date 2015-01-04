fceda58efd253360cb137fc29f0b4439a18f4393
CGY <c4bbage@qq.com>
2015-01-04 17:41:49 +0800

    修改autonmap

--- autonmap.py
+++ autonmap.py
@@ -39,52 +39,6 @@ class scanopt(threading.Thread):
         for host in self.portscan.all_hosts():
             for i in ([(x,self.portscan[host]['tcp'][x]['name']) for x in self.portscan[host]['tcp'].keys()]):
                 print ":".join((host,str(i[0]),str(i[1])))
-            #self.ret[host]=self.portscan[host].state(),[(x,self.portscan[host]['tcp'][x]['name']) for x in self.portscan[host]['tcp'].keys()]
-            # print [(x,self.portscan[host][x].keys()) for x in self.portscan[host].all_protocols()]
-            # for i in x in self.portscan[host].all_protocols():
-            #   print x,self.portscan[host][x]
-        # return self.ret
-        # for host in self.ret.keys():
-        #     #print host,self.ret[host]
-        #     for i in self.ret[host][1]:
-        #         if(i[0]==22):
-        #             print host+":"+str(22)+":ssh"+"\n"
-        #         elif(i[0]==21):
-        #             print host+":"+str(21)+":ftp"+"\n"
-        #         elif(i[0]==80):
-        #             print host+":"+str(80)+":http"+"\n"
-        #         elif(i[0]==443):
-        #             print host+":"+str(443)+":http"+"\n"
-        #         elif(i[0]==873):
-        #             print host+":"+str(i[0])+"rsync\n"
-        #         elif(i[0]==1433):
-        #             print host+":"+str(1433)+":mssql"+"\n"        
-        #         elif(i[0]==3306):
-        #             print host+":"+str(3306)+":mysql"+"\n"
-        #         elif(i[0]==3389):
-        #             print host+":"+str(3389)+":rdp"+"\n"
-        #         elif(i[0]==8000):
-        #             print host+":"+str(8000)+":http"+"\n"
-        #         elif(i[0]==8080):
-        #             print host+":"+str(8080)+":http"+"\n"
-        #         elif(i[0]==8888):
-        #             print host+":"+str(8888)+":http"+"\n"
-        #         elif(i[0]==27017):
-        #             print host+":"+str(27017)+":mongodb"+"\n"
-        #         elif(i[0]==11211):
-        #             print host+":"+str(11211)+":memcache"+"\n"
-        #         else:
-        #             pass
-        #self.logtofile(resline)
-        #print resline
-        # self.ret = self.proc.stdout.readlines() 
-        # if len(self.ret) == 3: 
-        #     if "SUCCESS" in self.ret[2]: 
-        #         self.lock.acquire() 
-        #         print self.host, self.port, self.ret[2] 
-        #         self.lock.release() 
-        # stdmsg, errmsg = self.proc.communicate() 
-        # self.timer.cancel() 
         self.lock.release()
 
 if __name__=='__main__': 
